80d05f9cbbc1564aaa16832ecb4d697482abfbbd
Erik Berkun-Drevnig <erik.b22@gmail.com>
2022-10-17 15:49:36 -0400

    Adding integration tests

--- integration/tests/basic_query_spec.lua
+++ integration/tests/basic_query_spec.lua
@@ -1,13 +1,13 @@
 require "busted.runner"()
 local lua_cassandra = require("luacassandra")
 
-describe("basic query", function() 
+describe("basic query", function()
     it("runs", function()
         assert.is_true(1 == 1)
     end)
-    
+
     it("lua_cassandra", function()
-        -- lua_cassandra.connect("127.0.0.1")
+        lua_cassandra.connect("127.0.0.1")
         -- lua_cassandra.query("", {})
     end)
 end)
