07a2dc07d48dc80517d556c9dcc2e0f219491487
Erik Berkun-Drevnig <erik.berkundrevnig@lotusflare.com>
2022-10-17 19:05:37 -0400

    Update basic_query_spec.lua

--- integration/tests/basic_query_spec.lua
+++ integration/tests/basic_query_spec.lua
@@ -4,7 +4,7 @@ local lucas = require("luacassandra")
 
 describe("luacassandra", function()
     it("can connect", function()
-        local err = lucas.connect("cassandra")
+        local err = lucas.connect("127.0.0.1")
         assert.is_nil(err)
     end)
 end)
