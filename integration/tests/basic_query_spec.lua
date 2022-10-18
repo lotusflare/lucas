3b39e62cf234e4d828beb44374bf6117dfc09858
Erik Berkun-Drevnig <erik.b22@gmail.com>
2022-10-18 01:53:53 -0400

    Improve tests, enable buildkit

--- integration/tests/basic_query_spec.lua
+++ integration/tests/basic_query_spec.lua
@@ -17,7 +17,16 @@ describe("luacassandra", function()
 
     it("select statement", function ()
         lucas.connect("127.0.0.1")
-        local results = lucas.execute_query("SELECT * FROM testing.data", {})
-        pretty.dump(results)
+        local results = lucas.query("SELECT * FROM testing.data", {})
+        assert.are.same(results, {
+            {
+                approval_status = 2,
+                asset_id = "015e3714-a98b-11ec-9f51-0242ac150008",
+                asset_type = 1,
+                id = "9380816255dc45dfa1a57541db81df1d",
+                id_type = 1,
+                operator_name = "avantel"
+            }
+        })
     end)
 end)
--- src/library.c
+++ src/library.c
@@ -35,24 +35,17 @@ static int connect(lua_State *L)
     return 0;
 }
 
-static int query(lua_State *L)
+void bind_number(CassStatement *statement, size_t index, CassValueType cass_type, lua_Number value)
 {
-    const int QUERY_POSITION = 1;
-    const int PARAMETERS_POSITION = 2;
-    const int param_count = lua_objlen(L, PARAMETERS_POSITION);
-    const char *query = lua_tostring(L, QUERY_POSITION);
-    CassStatement *statement = cass_statement_new(query, param_count);
-    CassFuture *future = cass_session_execute(session, statement);
-
-    for (int i = 0; i < param_count; ++i)
+    CassError err = cass_statement_bind_int32(statement, index, value);
+    if (err != CASS_OK)
     {
-        lua_rawgeti(L, PARAMETERS_POSITION, i + 1);
-        int current = lua_gettop(L);
+        const char *desc = cass_error_desc(err);
+        fprintf(stderr, "error: %s\n", desc);
     }
-    return 1;
 }
 
-int execute_query(lua_State *L)
+static int query(lua_State *L)
 {
     const int QUERY_POSITION = 1;
     const int PARAMETERS_POSITION = 2;
@@ -62,23 +55,12 @@ int execute_query(lua_State *L)
     luaL_checktype(L, QUERY_POSITION, LUA_TSTRING);
     luaL_checktype(L, PARAMETERS_POSITION, LUA_TTABLE);
 
-    if (session == NULL)
-    {
-        fprintf(stderr, "making new session\n");
-        session = cass_session_new();
-        cluster = cass_cluster_new();
-        cass_cluster_set_contact_points(cluster, getenv("CASSANDRA_DB"));
-        CassFuture *connect_future = cass_session_connect(session, cluster);
-        cass_future_wait(connect_future);
-        cass_future_free(connect_future);
-    }
-
     // Create statement
-    const int param_count = lua_objlen(L, 2);
+    const size_t param_count = lua_objlen(L, 2);
     const char *query = lua_tostring(L, QUERY_POSITION);
+    printf("creating statement `%s` with %ld parameter(s)\n", query, param_count);
     CassStatement *statement = cass_statement_new(query, param_count);
 
-    // Bind parameters to statement
     for (size_t i = 0; i < param_count; i++)
     {
         lua_rawgeti(L, PARAMETERS_POSITION, i + 1);
@@ -86,56 +68,22 @@ int execute_query(lua_State *L)
         lua_rawgeti(L, current, TYPE_POSITION);
         lua_rawgeti(L, current, VALUE_POSITION);
         const char *type = lua_tostring(L, -2);
-        CassError err;
+        const lua_Integer value = lua_tointeger(L, -1);
+        printf("type=%s value=%ld\n", type, value);
 
-        if (strcmp(type, "CASS_VALUE_TYPE_INT") == 0)
-        {
-            const lua_Integer value = lua_tointeger(L, -1);
-            err = cass_statement_bind_int32(statement, i, value);
-        }
-        else if (strcmp(type, "CASS_VALUE_TYPE_VARCHAR") == 0)
-        {
-            const char *value = lua_tostring(L, -1);
-            err = cass_statement_bind_string(statement, i, value);
-        }
-        else if (strcmp(type, "CASS_VALUE_TYPE_DOUBLE") == 0)
-        {
-            const lua_Number value = lua_tonumber(L, -1);
-            err = cass_statement_bind_double(statement, i, value);
-        }
-        // print_err(err);
+        bind_number(statement, i, CASS_VALUE_TYPE_INT, value);
         lua_pop(L, 2);
     }
 
-    // Execute statement
     CassFuture *execute_future = cass_session_execute(session, statement);
     cass_future_wait(execute_future);
-    CassError err = cass_future_error_code(execute_future);
-    // print_err(err);
-
-    const CassResult *execute_result;
-    size_t row_count;
-    size_t col_count;
-
-    if (err == CASS_OK)
-    {
-        execute_result = cass_future_get_result(execute_future);
-        row_count = cass_result_row_count(execute_result);
-        col_count = cass_result_column_count(execute_result);
-        fprintf(stderr, "results: rows=%ld, cols=%ld\n", row_count, col_count);
-    }
-    else
-    {
-        const char *msg;
-        size_t len;
-        cass_future_error_message(execute_future, &msg, &len);
-        fprintf(stderr, "error: %s\n", msg);
-        cass_future_free(execute_future);
-        return 0;
-    }
-
-    // Prepare for receiving results
+    printf("done executing\n");
+    const CassResult *execute_result = cass_future_get_result(execute_future);
     CassIterator *iterator = cass_iterator_from_result(execute_result);
+    size_t row_count = cass_result_row_count(execute_result);
+    size_t col_count = cass_result_column_count(execute_result);
+    printf("results: rows=%ld, cols=%ld\n", row_count, col_count);
+
     lua_newtable(L);
     int main_table = lua_gettop(L);
     int table_index = 1;
@@ -143,6 +91,7 @@ int execute_query(lua_State *L)
     while (cass_iterator_next(iterator) == cass_true)
     {
         const CassRow *row = cass_iterator_get_row(iterator);
+
         lua_pushinteger(L, table_index++);
         lua_newtable(L);
         int sub_table = lua_gettop(L);
@@ -150,6 +99,7 @@ int execute_query(lua_State *L)
         for (size_t c = 0; c < col_count; c++)
         {
             const CassValue *cass_value = cass_row_get_column(row, c);
+
             const char *col_name;
             size_t col_name_len;
             cass_result_column_name(execute_result, c, &col_name, &col_name_len);
@@ -163,70 +113,59 @@ int execute_query(lua_State *L)
             size_t str_len;
             char uuid_str_val[CASS_UUID_STRING_LENGTH];
             CassUuid uuid_val;
+            cass_int32_t int32_val;
             cass_bool_t bool_val;
             cass_double_t double_val;
-            cass_float_t float_val;
-            cass_int64_t int64_val;
-            cass_int32_t int32_val;
-            cass_int16_t int16_val;
-            cass_int8_t int8_val;
+            CassError err;
 
-            lua_pushstring(L, terminated);
             switch (vt)
             {
-            case CASS_VALUE_TYPE_TEXT:
             case CASS_VALUE_TYPE_VARCHAR:
                 cass_value_get_string(cass_value, &str_val, &str_len);
+                lua_pushstring(L, terminated);
                 lua_pushstring(L, str_val);
+                lua_settable(L, sub_table);
                 continue;
             case CASS_VALUE_TYPE_UUID:
             case CASS_VALUE_TYPE_TIMEUUID:
                 cass_value_get_uuid(cass_value, &uuid_val);
                 cass_uuid_string(uuid_val, uuid_str_val);
+                lua_pushstring(L, terminated);
                 lua_pushstring(L, uuid_str_val);
-                continue;
-            case CASS_VALUE_TYPE_TINY_INT:
-                cass_value_get_int8(cass_value, &int8_val);
-                lua_pushinteger(L, int8_val);
+                lua_settable(L, sub_table);
                 continue;
             case CASS_VALUE_TYPE_SMALL_INT:
-                cass_value_get_int16(cass_value, &int16_val);
-                lua_pushinteger(L, int16_val);
+            case CASS_VALUE_TYPE_TINY_INT:
+            case CASS_VALUE_TYPE_BIGINT:
                 continue;
             case CASS_VALUE_TYPE_INT:
                 cass_value_get_int32(cass_value, &int32_val);
+                lua_pushstring(L, terminated);
                 lua_pushinteger(L, int32_val);
-                continue;
-            case CASS_VALUE_TYPE_BIGINT:
-                cass_value_get_int64(cass_value, &int64_val);
-                lua_pushinteger(L, int64_val);
+                lua_settable(L, sub_table);
                 continue;
             case CASS_VALUE_TYPE_BOOLEAN:
                 cass_value_get_bool(cass_value, &bool_val);
+                lua_pushstring(L, terminated);
                 lua_pushboolean(L, bool_val);
+                lua_settable(L, sub_table);
                 continue;
             case CASS_VALUE_TYPE_DOUBLE:
                 cass_value_get_double(cass_value, &double_val);
+                lua_pushstring(L, terminated);
                 lua_pushnumber(L, double_val);
-                continue;
-            case CASS_VALUE_TYPE_FLOAT:
-                cass_value_get_float(cass_value, &float_val);
-                lua_pushnumber(L, float_val);
+                lua_settable(L, sub_table);
                 continue;
             default:
-                fprintf(stderr, "could not find type for column: %s\n", terminated);
+                printf("could not find type for column: %s\n", terminated);
+                lua_pushstring(L, terminated);
                 lua_pushnil(L);
+                lua_settable(L, sub_table);
                 continue;
             }
-            lua_settable(L, sub_table);
         }
         lua_settable(L, main_table);
     }
-
-    cass_iterator_free(iterator);
-    cass_result_free(execute_result);
-    cass_statement_free(statement);
-
     return 1;
 }
 
@@ -236,7 +175,6 @@ int luaopen_luacassandra(lua_State *L)
     luaL_Reg reg[] = {
         {"connect", connect},
         {"query", query},
-        {"execute_query", execute_query},
         {NULL, NULL},
     };
     luaL_openlib(L, "luacassandra", reg, 0);
