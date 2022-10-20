e617d5b28606c2d33d0836bc2ba5fc12d23ac746
Erik Berkun-Drevnig <erik.b22@gmail.com>
2022-10-20 19:13:21 -0400

    Fix

--- test-all.sh
+++ test-all.sh
@@ -1,20 +1,7 @@
 #!/bin/sh
 
-set -e
-trap clean EXIT
-
-pushd integration
-
-docker compose rm -fs
-docker compose up cassandra --wait
-
-docker compose exec cassandra cqlsh -f /init.cql
-docker compose exec driver busted --output=TAP .
-
 function clean {
-    echo "cleaning up"
-    docker compose rm -fs
-    popd
+    popd > /dev/null
     if [[ ${exit_code} -ne 0 ]]; then
         echo "fail"
     fi
@@ -22,3 +9,14 @@ function clean {
         echo "pass"
     fi
 }
+
+set -ex
+trap clean EXIT
+pushd integration > /dev/null
+
+docker compose rm -fs
+docker compose up cassandra --wait
+
+docker compose exec cassandra cqlsh -f /init.cql
+sleep 10
+docker compose exec driver busted --output=TAP .
