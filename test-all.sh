bb8a201ab6ae253cabbc9bb99bb4dc25529d278e
Erik Berkun-Drevnig <erik.b22@gmail.com>
2022-10-20 20:48:21 -0400

    Use docker-compose dev container

--- test-all.sh
+++ test-all.sh
@@ -1,20 +1,13 @@
-#!/bin/sh
+#!/bin/sh -ex
 
-function clean {
+function clean() {
     popd > /dev/null
-    if [[ ${exit_code} -ne 0 ]]; then
-        echo "fail"
-    fi
-    if [[ ${exit_code} -eq 0 ]]; then
-        echo "pass"
-    fi
 }
 
-set -ex
 trap clean EXIT
 pushd integration > /dev/null
 
-docker compos build
+docker compose build
 docker compose rm -fs
 docker compose up cassandra --wait
 cat init.cql | docker compose exec -T cassandra cqlsh
