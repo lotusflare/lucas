8b6d35abe0b8601cd8d2689e8f5ed8408b297217
Erik Berkun-Drevnig <erik.b22@gmail.com>
2022-11-15 15:37:45 -0500

    Add pthread

--- src/logging.c
+++ src/logging.c
@@ -4,6 +4,7 @@
 #include "luajit-2.1/lauxlib.h"
 #include "luajit-2.1/lua.h"
 #include "state.c"
+#include <pthread.h>
 #include <stdarg.h>
 #include <time.h>
 
@@ -109,15 +110,21 @@ int metrics(lua_State *L)
 
 void callback(const CassLogMessage *log, void *data)
 {
-    // lua_pushvalue(log_context, 1);
-    // lua_pushstring(log_context, log->message);
-    // lua_pushinteger(log_context, log->severity);
-    // lua_pushinteger(log_context, log->time_ms / 1000);
-    // lua_call(log_context, 3, 0);
+    pthread_mutex_lock(&lock);
+    lua_pushvalue(log_context, 1);
+    lua_pushstring(log_context, log->message);
+    lua_pushinteger(log_context, log->severity);
+    lua_pushinteger(log_context, log->time_ms / 1000);
+    lua_call(log_context, 3, 0);
+    pthread_mutex_unlock(&lock);
 }
 
 int logger(lua_State *L)
 {
+    if (log_context != NULL)
+    {
+        lua_close(log_context);
+    }
     luaL_checktype(L, 1, LUA_TFUNCTION);
     log_context = lua_newthread(L); // thread safety
     luaL_ref(L, LUA_REGISTRYINDEX);
@@ -129,24 +136,24 @@ int logger(lua_State *L)
 
 void lucas_log(CassLogLevel level, const char *fmt, ...)
 {
-    // if (log_context == NULL)
-    // {
-    //     return;
-    // }
+    if (log_context == NULL)
+    {
+        return;
+    }
 
-    // va_list args1, args2;
-    // va_start(args1, fmt);
-    // va_copy(args2, args1);
+    va_list args1, args2;
+    va_start(args1, fmt);
+    va_copy(args2, args1);
 
-    // char append[vsnprintf(NULL, 0, fmt, args1)];
-    // vsprintf(append, fmt, args2);
+    char append[vsnprintf(NULL, 0, fmt, args1)];
+    vsprintf(append, fmt, args2);
 
-    // lua_pushvalue(log_context, 1);
-    // lua_pushfstring(log_context, "lucas: %s", append);
-    // lua_pushinteger(log_context, level);
-    // lua_pushinteger(log_context, (int)time(NULL));
-    // lua_call(log_context, 3, 0);
+    lua_pushvalue(log_context, 1);
+    lua_pushfstring(log_context, "lucas: %s", append);
+    lua_pushinteger(log_context, level);
+    lua_pushinteger(log_context, (int)time(NULL));
+    lua_call(log_context, 3, 0);
 
-    // va_end(args1);
-    // va_end(args2);
+    va_end(args1);
+    va_end(args2);
 }
--- src/state.c
+++ src/state.c
@@ -2,6 +2,8 @@
 
 #include "cassandra.h"
 #include <luajit-2.1/lua.h>
+#include <pthread.h>
 
 CassSession *session = NULL;
 lua_State *log_context = NULL;
+pthread_mutex_t lock;
