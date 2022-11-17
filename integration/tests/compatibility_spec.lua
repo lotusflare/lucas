861a078e2902431961b5d21f7671db0a136cadcf
Erik Berkun-Drevnig <erik.b22@gmail.com>
2022-11-17 18:08:43 -0500

    Run formatter

--- integration/tests/compatibility_spec.lua
+++ integration/tests/compatibility_spec.lua
@@ -6,8 +6,6 @@ local compat = require("lucas.compatibility")
 describe("compatibility", function()
     it("convert string", function()
         local result = compat.convert("hello")
-        assert.array.is({
-            13, "hello"
-        }, result)
+        assert.array.is({ 13, "hello" }, result)
     end)
 end)
