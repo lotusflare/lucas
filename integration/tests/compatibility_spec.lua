5d61ddeb741971d6f8da6db03026d2252d896377
Erik Berkun-Drevnig <erik.b22@gmail.com>
2022-11-21 20:33:01 -0500

    Fix test

--- integration/tests/compatibility_spec.lua
+++ integration/tests/compatibility_spec.lua
@@ -3,26 +3,10 @@ local pretty = require("pl.pretty")
 local lucas = require("lucas")
 local compat = require("lucas.compatibility")
 
-local function convert_type(value)
-    local t = type(value)
-    if (t == "string") then
-        return lucas.varchar(value)
-    elseif (t == "boolean") then
-        return lucas.boolean(value)
-    elseif (t == "number") then
-        return lucas.int(value)
-    elseif (t == "table") then
-        if (value.__cql_type == nil) then
-            return lucas.null()
-        end
-        return { value.__cql_type, value.val }
-    end
-end
-
 describe("compatibility", function()
     it("convert string", function()
         local result = compat.convert("hello")
-        assert.are.same(convert_type("hello"), result)
+        assert.are.same(lucas.varchar("hello"), result)
     end)
 
     it("convert int", function()
@@ -32,7 +16,7 @@ describe("compatibility", function()
 
     it("convert bool", function()
         local result = compat.convert(true)
-        assert.are.same(convert_type(true), result)
+        assert.are.same(lucas.boolean(true), result)
     end)
 
     it("convert table", function()
