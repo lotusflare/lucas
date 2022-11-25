02acae36cd0ea0edfc68b1865792a4bf2e456c69
Erik Berkun-Drevnig <erik.b22@gmail.com>
2022-11-25 18:37:21 -0500

    Tweaks

--- src/connect.c
+++ src/connect.c
@@ -9,23 +9,21 @@
 int get_port(lua_State *L, int i)
 {
     lua_getfield(L, i, "port");
-    int port = lua_tointeger(L, lua_gettop(L));
-    if (port == 0)
+    if (lua_type(L, i) == LUA_TNIL)
     {
         return 9042;
     }
-    return port;
+    return lua_tointeger(L, lua_gettop(L));
 }
 
 int get_num_threads_io(lua_State *L, int i)
 {
     lua_getfield(L, i, "num_threads_io");
-    int num = lua_tointeger(L, lua_gettop(L));
-    if (num == 0)
+    if (lua_type(L, i) == LUA_TNIL)
     {
         return 1;
     }
-    return num;
+    return lua_tointeger(L, lua_gettop(L));
 }
 
 bool get_use_latency_aware_routing(lua_State *L, int i)
@@ -41,18 +39,30 @@ bool get_use_latency_aware_routing(lua_State *L, int i)
 const char *get_contact_points(lua_State *L, int i)
 {
     lua_getfield(L, i, "contact_points");
+    if (lua_type(L, i) == LUA_TNIL)
+    {
+        return "127.0.0.1";
+    }
     return lua_tostring(L, lua_gettop(L));
 }
 
 const char *get_application_name(lua_State *L, int i)
 {
     lua_getfield(L, i, "application_name");
+    if (lua_type(L, i) == LUA_TNIL)
+    {
+        return NULL;
+    }
     return lua_tostring(L, lua_gettop(L));
 }
 
 bool get_reconnect(lua_State *L, int i)
 {
     lua_getfield(L, i, "reconnect");
+    if (lua_type(L, i) == LUA_TNIL)
+    {
+        return false;
+    }
     return lua_toboolean(L, lua_gettop(L));
 }
 
@@ -60,14 +70,18 @@ static int connect(lua_State *L)
 {
     lucas_log(CASS_LOG_INFO, "Attempting to connect");
     const int ARG_OPTIONS = 1;
+    CassFuture *future = NULL;
+    LucasError *rc = NULL;
+    CassCluster *cluster = NULL;
+    CassError err = CASS_OK;
+
     const char *contact_points = get_contact_points(L, ARG_OPTIONS);
     const int port = get_port(L, ARG_OPTIONS);
     const int num_threads_io = get_num_threads_io(L, ARG_OPTIONS);
     const char *application_name = get_application_name(L, ARG_OPTIONS);
-    cass_bool_t use_latency_aware_routing = get_use_latency_aware_routing(L, ARG_OPTIONS);
+    const bool use_latency_aware_routing = get_use_latency_aware_routing(L, ARG_OPTIONS);
     const bool reconnect = get_reconnect(L, ARG_OPTIONS);
-    CassFuture *future = NULL;
-    LucasError *rc = NULL;
+
     if (!reconnect && session != NULL)
     {
         lucas_log(CASS_LOG_INFO, "already connected");
@@ -78,8 +92,8 @@ static int connect(lua_State *L)
         cass_session_free(session);
     }
     session = cass_session_new();
-    CassCluster *cluster = cass_cluster_new();
-    CassError err = cass_cluster_set_contact_points(cluster, contact_points);
+    cluster = cass_cluster_new();
+    err = cass_cluster_set_contact_points(cluster, contact_points);
     if (err != CASS_OK)
     {
         rc = lucas_new_errorf_from_cass_error(err, "could not set contact points %s", contact_points);
@@ -112,7 +126,7 @@ static int connect(lua_State *L)
     err = cass_future_error_code(future);
     if (err != CASS_OK)
     {
-        rc = lucas_new_errorf_from_cass_error(err, "could not connect to %s", contact_points);
+        rc = lucas_new_errorf_from_cass_error(err, "could not connect to cluster");
         goto cleanup;
     }
 
