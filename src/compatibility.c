6136d42a04e3fdcea7d2935203487c10cf0d9cc8
Erik Berkun-Drevnig <erik.b22@gmail.com>
2022-12-31 20:37:53 -0500

    Revert "Refactor"
    This reverts commit 384e88ce606a5151953a5b740aa689741f559f77.

--- src/compatibility.c
+++ src/compatibility.c
@@ -11,6 +11,7 @@ LucasError *cast(lua_State *L, int value_index)
     lua_newtable(L);
     int table = lua_gettop(L);
     int type = lua_type(L, value_index);
+    LucasError *rc = NULL;
 
     if (type == LUA_TSTRING)
     {
@@ -35,10 +36,14 @@ LucasError *cast(lua_State *L, int value_index)
     }
     else if (type == LUA_TTABLE)
     {
-        return convert_table(L, value_index, table);
+        rc = convert_table(L, value_index, table);
+    }
+    else
+    {
+        return lucas_new_errorf("invalid type");
     }
 
-    return lucas_new_errorf("invalid type");
+    return NULL;
 }
 
 LucasError *convert_list(lua_State *L, int index, int table, CassValueType *type)
@@ -102,28 +107,29 @@ LucasError *convert_list(lua_State *L, int index, int table, CassValueType *type
     return NULL;
 }
 
-bool table_empty(lua_State *L, int index)
-{
-    lua_pushnil(L);
-    if (lua_next(L, index) != 0)
-    {
-        lua_pop(L, 2);
-        return false;
-    }
-    return true;
-}
-
 LucasError *convert_table(lua_State *L, int index, int return_table)
 {
+    // printf("convert_table\n");
+
     LucasError *rc = NULL;
+
     // check if table is empty
-    bool is_empty = table_empty(L, index);
+    bool is_empty = true;
+    lua_pushnil(L);
+    for (int last_top = lua_gettop(L); lua_next(L, index) != 0; lua_pop(L, lua_gettop(L) - last_top))
+    {
+        is_empty = false;
+        break;
+    }
     if (is_empty)
     {
         lua_pushinteger(L, CASS_VALUE_TYPE_NULL);
         lua_rawseti(L, return_table, 1);
-        return rc;
+        // printf("is_empty top=%d\n", lua_gettop(L));
+        return NULL;
     }
+    lua_pop(L, 2);
+    // printf("not empty\n");
 
     // check if is CQL type
     lua_getfield(L, index, "__cql_type");
@@ -135,7 +141,8 @@ LucasError *convert_table(lua_State *L, int index, int return_table)
     {
         lua_getfield(L, index, "val");
         int val_index = lua_gettop(L);
-        rc = convert_list(L, val_index, return_table, &cvt);
+        // convert the table but pass the type override
+        convert_list(L, val_index, return_table, &cvt);
         lua_pop(L, 1);
         return NULL;
     }
@@ -147,6 +154,7 @@ LucasError *convert_table(lua_State *L, int index, int return_table)
         lua_rawseti(L, return_table, 2);
         return NULL;
     }
+    // printf("not __cql_type\n");
 
     // type is unknown so pass NULL
     rc = convert_list(L, index, return_table, NULL);
@@ -159,6 +167,7 @@ static int convert(lua_State *L)
     const int ARG_PARAM = 1;
     int type = lua_type(L, ARG_PARAM);
     LucasError *rc = cast(L, ARG_PARAM);
+    // printf("top=%d\n", lua_gettop(L));
 
 done:
     if (rc)
