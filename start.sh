050732bfa8e15408b30454b3075141ce4ff29e7c
Erik Berkun-Drevnig <erik.b22@gmail.com>
2023-02-03 18:54:14 -0500

    ci: use luajit

--- start.sh
+++ start.sh
@@ -23,7 +23,7 @@ build() {
 test() {
     if $test; then
         print "Running tests"
-        docker compose run driver busted # --lua=luajit
+        docker compose run driver busted --lua=luajit
     fi
 }
 
