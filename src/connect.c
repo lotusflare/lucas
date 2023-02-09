06e46466038ff841e42235283057a3a076ec6a49
Erik Berkun-Drevnig <erik.b22@gmail.com>
2023-02-09 17:20:41 -0500

    refactor: move files

--- src/connect.c
+++ src/connect.c
@@ -1,4 +1,3 @@
-#include "auth.c"
 #include "cassandra.h"
 #include "errors.c"
 #include "logs.c"
@@ -181,6 +180,43 @@ LucasError *set_ssl(lua_State *L, int i, CassCluster *cluster)
 
 cleanup:
     cass_ssl_free(ssl);
+    return rc;
+}
+
+LucasError *set_authenticator(lua_State *L, int i, CassCluster *cluster)
+{
+    LucasError *rc = NULL;
+
+    lua_getfield(L, i, "credentials");
+    int credentials_index = lua_gettop(L);
+    if (lua_isnil(L, credentials_index))
+    {
+        lucas_log(LOG_WARN, "credentials not provided");
+        goto cleanup;
+    }
+
+    lua_getfield(L, credentials_index, "username");
+    if (lua_isnil(L, lua_gettop(L)))
+    {
+        rc = lucas_new_errorf("username is missing");
+        goto cleanup;
+    }
+    const char *username = lua_tostring(L, lua_gettop(L));
+    lucas_log(LOG_DEBUG, "setting username=%s", username);
+
+    lua_getfield(L, credentials_index, "password");
+    if (lua_isnil(L, lua_gettop(L)))
+    {
+        rc = lucas_new_errorf("password is missing");
+        goto cleanup;
+    }
+    const char *password = lua_tostring(L, lua_gettop(L));
+    lucas_log(LOG_DEBUG, "setting password=%s", password);
+
+    cass_cluster_set_credentials(cluster, username, password);
+    lucas_log(LOG_INFO, "authenticator configured");
+
+cleanup:
     return rc;
 }
 
